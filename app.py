5faacab9effc83440ddf774e4b053dacab656a35
Abhishek Thorat <83878982+abmath113@users.noreply.github.com>
2023-04-17 09:28:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ PORT = 587
 EMAIL_ID = "2020.shreya.singh@ves.ac.in"
 PASSWORD = "gyhbuntzprvskqnv"
 
+
 app = Flask(__name__)
 
 def notify(url):
@@ -29,7 +30,7 @@ def track_price():
     afford_price = float(request.form['affordable'])
     page = requests.get(url, headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"})
     soup = bs(page.content, "html.parser")
-    val = soup.find(class_ = "a-offscreen").text.split()[0].replace(",", "")
+    val = soup.find(class_ = "a-price-whole").text.split()[0].replace(",", "")
     val = val.split()[0].replace("₹", "")
     price = float(val)
     if price <= afford_price :
